3773279a2dfafca2527a77e6c5fe024bfec11a7f
Mariwan <sehadmarewan@gmail.com>
2024-08-13 17:02:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Experience from "./components/Experience"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Portfolio from "./components/Portfolio"
+import ShinyEffect from "./components/ShinyEffect"
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Portfolio />
+      <Experience />
+      <About />
+      <Contact />
+      <Footer />
+    </> 
+  )
+}
+
+export default App
